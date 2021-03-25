b93ab6ba327420f1cb189082e0017204c15b6bda
mkovalev <mkovalevhse@yandex.ru>
2021-03-26 00:22:09 +0300

    доделать модальное окно

--- src/Pages/AdminPage/CreatePollPage.js
+++ src/Pages/AdminPage/CreatePollPage.js
@@ -1,30 +1,41 @@
 import Nav from "../../components/Nav";
 import ProfileStaff from "../../components/ProfileStaff";
 import {Content} from "antd/es/layout/layout";
-import {Button, Divider, Form, Input, InputNumber, Select, Space} from "antd";
-import {LeftOutlined, MinusCircleOutlined, PlusOutlined,} from "@ant-design/icons";
+import {Button, DatePicker, Divider, Form, Input, Select, Switch} from "antd";
+import {LeftOutlined,} from "@ant-design/icons";
 import {useHistory} from "react-router-dom";
 import TextArea from "antd/es/input/TextArea";
 import {Option} from "antd/es/mentions";
 import {useState} from "react";
+import ModalQuestions from "../../components/ModalQuestions";
+
 
 
 function CreatePollPage(){
 
     const history = useHistory();
-    const [categoryset, setCategorySet] = useState()
+
+    const [latePosting, setLatePosting] = useState()
 
     function handleClick(e) {
             console.log(e)
             history.push(e);
             }
 
-            const onFinish = (values: any) => {
-                console.log(values);
-            };
+    const onFinish = (values: any) => {
+        console.log(values);
+
+    };
+
     function handleChange(value) {
-        setCategorySet(value)
+
+
     }
+    function onChange(checked) {
+        setLatePosting(checked)
+    }
+
+
 
     return(
         <>
@@ -51,70 +62,27 @@ function CreatePollPage(){
                        <Option value='service'>Службы</Option>
                    </Select>
                 </Form.Item>
+                <Form.Item>
 
-                {categoryset==='participant'?
-                    <Form.List name="questions">
-                        {(fields, { add, remove }) => (
-                              <>
-                                    {fields.map(field => (
-                                          <Space key={field.key} style={{ display: 'flex', marginBottom: 8 }} align="baseline">
-                                              <span>{field.key + 1}</span>
-                                                <Form.Item
-                                                  {...field}
-                                                  name={[field.name, 'question']}
-                                                  fieldKey={[field.fieldKey, 'question']}
-                                                >
-                                                  <Input placeholder="Вопрос" />
-                                                </Form.Item>
+                </Form.Item>
 
-                                                <Form.Item label='Укажите вариант ответа'
-                                                           name={[field.name, 'answer']}
-                                                           fieldKey={[field.fieldKey, 'answer']}>
-                                                        <InputNumber defaultValue={0} min={5} max={100} step={5}/>
-                                                </Form.Item>
-                                                <MinusCircleOutlined onClick={() => remove(field.name)} />
-                                          </Space>
-                                    ))}
-                                    <Form.Item>
-                                      <Button type="dashed" onClick={() => add()} block icon={<PlusOutlined />}>
-                                        Добавить вопрос
-                                      </Button>
-                                    </Form.Item>
-                              </>
-                        )}
-                    </Form.List>
-                    :
-                <Form.List name="questions">
-                        {(fields, { add, remove }) => (
-                              <>
-                                    {fields.map(field => (
-                                          <Space key={field.key} style={{ display: 'flex', marginBottom: 8 }} align="baseline">
-                                                <Form.Item name={[field.name, 'question']}>
-                                                  <Input placeholder="Вопрос" />
-                                                </Form.Item>
-                                                <MinusCircleOutlined onClick={() => remove(field.name)} />
-                                          </Space>
-                                    ))}
+                <Form.Item name='latePosting' label='Отложный постинг:'>
+                    <Switch defaultUnChecked onChange={onChange} />
+                </Form.Item>
+                    {latePosting?
+                        <Form.Item name='datePosting'>
+                            <DatePicker showTime format="DD/MM/YYYY HH:mm" />
+                        </Form.Item>:null}
 
-                                    <Form.Item>
-                                      <Button type="dashed" onClick={() => add()} block icon={<PlusOutlined />}>
-                                        Добавить вопрос
-                                      </Button>
-                                    </Form.Item>
-
-                              </>
-                        )}
-                    </Form.List>}
-
-                    <Form.Item>
-                        <Button type="primary" htmlType="submit" block>
-                          Создать
-                        </Button>
-                      </Form.Item>
+                <Form.Item>
+                    <Button type="primary" htmlType="submit" block>
+                      Создать
+                    </Button>
+                  </Form.Item>
 
 
             </Form>
-
+            <ModalQuestions />
         </Content>
         </>
     )
