2c87dcc9598f9b000d69cbe100f4078479779163
mkovalev-python <mkovalevhse@yandex.ru>
2021-04-09 17:49:07 +0300

    16

--- src/Pages/AdminPage/AnalyticsPage.js
+++ src/Pages/AdminPage/AnalyticsPage.js
@@ -1,16 +1,74 @@
-import {Divider} from "antd";
+import {Divider, Table} from "antd";
 import Nav from "../../components/Nav";
 import ProfileStaff from "../../components/ProfileStaff";
 import {Content} from "antd/es/layout/layout";
+import {useEffect} from "react";
+import API from "../../API";
 
 function AnalyticsPage(){
+
+    const columns_logger = [
+      {
+        title: 'Участник',
+        dataIndex: 'name',
+        key: 'name'
+      },
+      {
+        title: 'Баллы',
+        dataIndex: 'points',
+        key: 'points'
+      },
+      {
+        title: 'Дата',
+        dataIndex: 'date',
+        key: 'date'
+      },
+
+    ]
+    const columns_rating = [
+      {
+        title: 'Позиция',
+        dataIndex: 'position',
+        key: 'position'
+      },
+      {
+        title: 'Участник',
+        dataIndex: 'name',
+        key: 'name'
+      },
+     {
+        title: 'Баллов',
+        dataIndex: 'points',
+        key: 'points'
+      },]
+
+    useEffect(()=>{
+        API.get('/get/analytics/', {
+            headers: {'Authorization': "JWT " + sessionStorage.getItem('token')}
+        })
+            .then(req=>{
+                console.log(req.data)
+            })
+            .catch(error=>{
+                console.log(error.request)
+            })
+    },[])
+
     return(
         <>
             <Nav />
             <ProfileStaff/>
             <Content>
                 <Divider>Аналитика</Divider>
-                <h1>Аналитические данные</h1>
+                <div style={{ width: '45%', textAlign:'center', float:'left', marginRight: "10%"}}>
+                    <h3>Логгер баллов</h3>
+                    <Table columns={columns_logger} />
+                </div>
+                <div style={{ width: '45%', textAlign:'center', float:'left'}}>
+                    <h3>Таблица рейтинга участников</h3>
+                    <Table columns={columns_rating} />
+                </div>
+
             </Content>
         </>
     )
