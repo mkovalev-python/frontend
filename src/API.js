a58f34b79885be276942081fa2f2160f9b6c97e7
maximkovalev <mkovalevhse@yandex.ru>
2021-09-23 09:45:58 +0300

    cookies

--- src/API.js
+++ src/API.js
@@ -1,5 +1,9 @@
 import axios from "axios";
+import Cookies from "js-cookie"
 
 export default axios.create({
-    baseURL: '/api/v0/'
+    baseURL: '/api/v0/',
+    headers: {
+        'X-CSRFTOKEN': Cookies.get("csrftoken")
+    }
 });
